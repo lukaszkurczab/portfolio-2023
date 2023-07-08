105690bcdfcc100c81719d2b5c65ef3bc426911e
Łukasz Kurczab <lukasz.kurczab@gmail.com>
2023-07-08 23:54:38 +0200

    Nav styled

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,10 +3,25 @@ import styles from "./navigation.module.scss"
 export default function Footer() {
   return (
         <div className={styles.nav}>
-          <button className={styles.nav__button} >Home</button>
-          <button className={styles.nav__button} >Tech stack</button>
-          <button className={styles.nav__button} >Projects</button>
-          <button className={styles.nav__button} >Contact</button>
+          <div className={styles.nav__button}>
+            <span className={styles.nav__buttonBar} />
+            <a className={styles.nav__buttonText} >Home</a>
+          </div>
+          <span className={styles.nav__dot}/>
+          <div className={styles.nav__button}>
+            <span className={styles.nav__buttonBar} />
+            <a className={styles.nav__buttonText} >Tech stack</a>
+          </div>
+          <span className={styles.nav__dot}/>
+          <div className={styles.nav__button}>
+            <span className={styles.nav__buttonBar} />
+            <a className={styles.nav__buttonText} >Projects</a>
+          </div>
+          <span className={styles.nav__dot}/>
+          <div className={styles.nav__button}>
+            <span className={styles.nav__buttonBar} />
+            <a className={styles.nav__buttonText} >Contact</a>
+          </div>
         </div>
   )
 }

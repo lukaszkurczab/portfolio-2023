64e2daadc7764b0cded06069ed59565437a91f8d
Łukasz Kurczab <lukasz.kurczab@capgemini.com>
2023-07-11 12:54:38 +0200

    Experience page styles implemented

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -13,48 +13,48 @@ const Experience = () => {
 
   return (
     <main className={styles.landingPage}>
-      <h2 className="projects__title">Projects</h2>
-      <div className="projects__tiles">
-        <a href="https://booking-lkurczab.netlify.app/" className="project__tile">
-            <Image src={booking} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">Booking</h3>
-              <h2 className="project__tile-desc">Mapping an existing page</h2>
+      <h2 className={styles.experience__title}>Experience</h2>
+      <div className={styles.projects__tiles}>
+        <a href="https://booking-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={booking} alt="" className={styles.project__tileImage}/>
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>Booking</h3>
+              <h2 className={styles.project__tileDesc}>Mapping an existing page</h2>
             </div>
         </a>
-        <a href="https://quizapp-lkurczab.netlify.app/" className="project__tile">
-            <Image src={quiz} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">Quiz</h3>
-              <h2 className="project__tile-desc">Own project</h2>
+        <a href="https://quizapp-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={quiz} alt="" className={styles.project__tileImage}/>
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>Quiz</h3>
+              <h2 className={styles.project__tileDesc}>Own project</h2>
             </div>
         </a>
-        <a href="https://portfolio-lkurczab.netlify.app/" className="project__tile">
-            <Image src={portfolio} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">Portfolio</h3>
-              <h2 className="project__tile-desc">Created based on the project</h2>
+        <a href="https://portfolio-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={portfolio} alt="" className={styles.project__tileImage}/>
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>Portfolio</h3>
+              <h2 className={styles.project__tileDesc}>Created based on the project</h2>
             </div>
         </a>
-        <a href="https://youtube-lkurczab.netlify.app/" className="project__tile">
-            <Image src={youtube} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">YouTube</h3>
-              <h2 className="project__tile-desc">Mapping an existing page</h2>
+        <a href="https://youtube-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={youtube} alt="" className={styles.project__tileImage} />
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>YouTube</h3>
+              <h2 className={styles.project__tileDesc}>Mapping an existing page</h2>
             </div>
         </a>
-        <a href="https://fylo-lkurczab.netlify.app/" className="project__tile">
-            <Image src={fylo} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">Fylo</h3>
-              <h2 className="project__tile-desc">Created based on the project</h2>
+        <a href="https://fylo-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={fylo} alt="" className={styles.project__tileImage}/>
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>Fylo</h3>
+              <h2 className={styles.project__tileDesc}>Created based on the project</h2>
             </div>
         </a>
-        <a href="https://social-media-dashboard-lkurczab.netlify.app/" className="project__tile">
-            <Image src={socialMedia} alt="" className="project__tile-image"/>
-            <div className="project__tile-textWrapper">
-              <h3 className="project__tile-name">Social Media Description</h3>
-              <h2 className="project__tile-desc">Created based on the project</h2>
+        <a href="https://social-media-dashboard-lkurczab.netlify.app/" className={styles.project__tile}>
+            <Image src={socialMedia} alt="" className={styles.project__tileImage}/>
+            <div className={styles.project__tileTextWrapper}>
+              <h3 className={styles.project__tileName}>Social Media Description</h3>
+              <h2 className={styles.project__tileDesc}>Created based on the project</h2>
             </div>
         </a>
       </div>

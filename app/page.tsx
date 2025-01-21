93519ae6590e4830d70c3ae8c9a4882b842869ba
Lukasz Kurczab <lukasz.kurczab@gmail.com>
2025-01-21 10:55:21 +0100

    nav fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 
 export default function Page() {
   return (
     <main className={styles.landingPage}>
-      <h1><span className={styles.name}>Łukasz</span>Kurczab</h1>
+      <h1>
+        <span className={styles.name}>Łukasz</span>Kurczab
+      </h1>
       <h2>
-        Front-end <br/> 
+        Software <br />
         developer
       </h2>
-   </main>
-  )
+    </main>
+  );
 }
